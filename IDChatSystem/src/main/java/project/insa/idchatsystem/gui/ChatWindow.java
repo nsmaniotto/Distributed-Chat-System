b36b9431277da3942619d13d1980e26b240c1817
nsmaniotto <smaniotto.nathan@gmail.com>
2020-11-19 13:30:41 +0100

    implemented ChatWindow prototype structure

---
+++ IDChatSystem/src/main/java/project/insa/idchatsystem/gui/ChatWindow.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project.insa.idchatsystem.gui;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author nsmaniotto
+ */
+public class ChatWindow extends Window {
+    private JFrame chatFrame = new JFrame("IDChat");
+    
+    public ChatWindow() {
+        super("IDChat");
+    }
+    
+    @Override
+    public void display() {
+        this.frame.pack();
+        this.frame.setVisible(true);
+    }
+}
