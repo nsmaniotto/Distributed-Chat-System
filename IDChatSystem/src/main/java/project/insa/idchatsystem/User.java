966e1299e154bd1756cdede71409c3e01aa2e341
Ro Mo <robin.128.456@gmail.com>
2020-11-24 22:54:22 +0100

    Update user avec calcul de l'adresse ip réelle (pas loopback) UserModel non à jour

--- IDChatSystem/src/main/java/project/insa/idchatsystem/User.java
+++ IDChatSystem/src/main/java/project/insa/idchatsystem/User.java
@@ -3,9 +3,8 @@ package project.insa.idchatsystem;
 
 import project.insa.idchatsystem.Exceptions.Uninitialized;
 
-import java.net.UnknownHostException;
+import java.net.*;
 import java.sql.Timestamp;
-import java.net.InetAddress;
 
 class User {
 
@@ -16,60 +15,93 @@ class User {
     private final String ipAddress;
 
     private final Timestamp lastSeen;
-    private static  String current_username = "";
+    private static String current_username = "";
     private static int current_id;
     private static String current_ipAddress;
+
     /************************Current user methods***************************************/
-    public static void init_current_user(int id) throws UnknownHostException {
+    public static void init_current_user(int id) {
         User.current_id = id;
-        User.current_ipAddress = InetAddress.getLocalHost().getHostAddress();
+        User.current_ipAddress = User.calculate_current_ip();
+    }
+    private static String calculate_current_ip()  {
+        try(DatagramSocket socket = new DatagramSocket()){
+            socket.connect(InetAddress.getByName("5.6.7.8"), 10000);
+            return socket.getLocalAddress().getHostAddress();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
     public static void set_current_username(String username) throws Uninitialized {
-        if(User.current_id == -1){
+        if (User.current_id == -1) {
             throw new Uninitialized("CurrentUser not initialized");
         }
         User.current_username = username;
     }
-    public static  String get_current_username() throws Uninitialized {
-        if(User.current_id == -1){
+
+    public static String get_current_username() throws Uninitialized {
+        if (User.current_id == -1) {
             throw new Uninitialized("CurrentUser not initialized");
         }
         return User.current_username;
     }
-    public static int get_current_id () throws Uninitialized {
-        if(User.current_id == -1){
+
+    public static int get_current_id() throws Uninitialized {
+        if (User.current_id == -1) {
             throw new Uninitialized("CurrentUser not initialized");
         }
         return User.current_id;
     }
+
     public static String get_current_ipAddress() throws Uninitialized {
-        if(User.current_id == -1){
+        if (User.current_id == -1) {
             throw new Uninitialized("CurrentUser not initialized");
         }
         return User.current_ipAddress;
     }
+
     /***********************Other users methods***************************/
-    public User(String username,int id,String ipAddress){
+    public User(String username, int id, String ipAddress) {
         this.username = username;
         this.id = id;
         this.ipAddress = ipAddress;
         this.lastSeen = new Timestamp(System.currentTimeMillis());
     }
+
     public String get_username() {
-		return this.username;
-	}
+        return this.username;
+    }
+
     public int get_id() {
-		return this.id;
-	}
+        return this.id;
+    }
+
     public String get_ipAddress() {
-		return this.ipAddress;
-	}
+        return this.ipAddress;
+    }
+
     public Timestamp get_lastSeen() {
-		return this.lastSeen;
-	}
-	/*********************Utilities methods*****************************/
+        return this.lastSeen;
+    }
+
+    /*********************Utilities methods*****************************/
     @Override
     public String toString() {
-        return String.format("User %s ; id %d ; ipAddress %s ; lastSeen %s",this.username,this.id,this.ipAddress,this.lastSeen.toString());
+        return String.format("User %s ; id %d ; ipAddress %s ; lastSeen %s", this.username, this.id, this.ipAddress, this.lastSeen.toString());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof User))
+            return false;
+        User other = (User) o;
+        if (this.id == other.get_id())
+            return true;
+        else
+            return false;
     }
 }
--- IDChatSystem/src/main/java/project/insa/idchatsystem/UserModel.java
+++ IDChatSystem/src/main/java/project/insa/idchatsystem/UserModel.java
@@ -4,17 +4,37 @@ package project.insa.idchatsystem;
 import project.insa.idchatsystem.Exceptions.Uninitialized;
 
 import javax.swing.JList;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.regex.*;
 
-class UserModel implements ObservableUserModel {
+class UserModel implements Runnable {
     private String networkMode;
-    private ArrayList<User> users;
+    private HashMap<Integer,User> users;
+    protected MulticastSocket socket = null;
+    //Format of UDP packets : username,id,ipAddress with :
+    // - username : max 25 letters
+    // - id : between 0 and 1 000 000 > 100 000 -> between 1 and 7 digits
+    // - ipAdress : maximum 3*4 + 3 = 15 characters
+    // Result = 25 + 7 + 15 = 47 character -> we will round this number to 50 characters
+    protected byte[] buf = new byte[256];
+    int port_broadcast = 4000;
 
     public UserModel(String networkMode, int id) throws UnknownHostException {//TODO : revoir si exception à gérer plus haut ou pas
         this.networkMode = networkMode;
-        this.users = new ArrayList<User>();
+        this.users = new HashMap<Integer,User>();
         User.init_current_user(id);
+
+        InetAddress group = null;
+        try {
+            group = InetAddress.getByName("230.0.0.0");
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
     }
 
     public void setUsername(String newUserName) {
@@ -26,24 +46,20 @@ class UserModel implements ObservableUserModel {
     }
 
     public void addOnlineUser(User user) {
-        this.users.add(user);
+        this.users.put(user.get_id(),user);//Replace automatically the previous version if already in the HashMap
     }
 
     public void removeOnlineUser(User user) {
-        int index_user = users.indexOf(user);
-        if(index_user==-1) {
+        if(this.users.remove(user.get_id()) == null) {
             System.out.printf("%s is not connected%n",user.toString());
         }
-        else {
-            this.users.remove(index_user);
-        }
     }
 
-    public User getOnlineUsers() {
-        return null;
+    public HashMap<Integer,User> getOnlineUsers() {
+        return this.users;
     }
 
-    public void difuseNewUsername(String username, int id) {
+    public void diffuseNewUsername(String username, int id) {
         
     }
 
@@ -52,17 +68,35 @@ class UserModel implements ObservableUserModel {
     }
 
     @Override
-    public void addObserver() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void deleteObserver() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void notifyObservers() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void run() {
+        //Création des objets pour communiquer sur le réseau de login
+        //Création des objets pour envoyer et recevoir de l'UDP en broadcast (multicast ici pour éviter de trop utiliser de ressources) des communications
+        try {
+            socket = new MulticastSocket(port_broadcast);
+            socket.setSoTimeout(0);//infinite timeout to block on receive
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Mutlicast socket error");
+        }
+        Pattern pattern_new_host = Pattern.compile("(?<username>[A-Za-z_.]+),(?<id>[0-9]+),(?<ip>[0-9]+[.][0-9]+[.][0-9]+[.][0-9]+)");
+        try {
+            socket.joinGroup(group);
+            while (true) {
+                DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                socket.receive(packet);
+                String received = new String(packet.getData(), 0, packet.getLength());
+                //Extraction of the informations of the packet thanks to a regex and named group
+                Matcher m = pattern_new_host.matcher(received);
+                while (m.find()){
+                    User new_user = new User(m.group("username"),Integer.parseInt(m.group("id")),m.group("ip"));
+                    this.addOnlineUser(new_user);//Add or refresh informations of the user based on the id
+                }
+            }
+            //socket.leaveGroup(group);
+            //socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Cannot join multicast group");
+        }
     }
 }
