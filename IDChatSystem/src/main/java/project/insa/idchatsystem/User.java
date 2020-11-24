4761999e7b5d5cfb37691fde15f46fa5b4e68600
Ro Mo <robin.128.456@gmail.com>
2020-11-24 16:18:04 +0100

    Mise à jour de la classe User

--- IDChatSystem/src/main/java/project/insa/idchatsystem/User.java
+++ IDChatSystem/src/main/java/project/insa/idchatsystem/User.java
@@ -1,16 +1,70 @@
 
 package project.insa.idchatsystem;
 
+import project.insa.idchatsystem.Exceptions.Uninitialized;
+
+import java.net.UnknownHostException;
 import java.sql.Timestamp;
+import java.net.InetAddress;
 
 class User {
-    private boolean current = false;
 
-    private String username;
+    private final String username;
 
-    private int id;
+    private int id = -1;
 
-    private String ipAddress;
+    private final String ipAddress;
 
-    private Timestamp lastSeen;
+    private final Timestamp lastSeen;
+    private static  String current_username = "";
+    private static int current_id;
+    private static String current_ipAddress;
+    /************************Current user methods***************************************/
+    public static void init_current_user(int id) throws UnknownHostException {
+        User.current_id = id;
+        User.current_ipAddress = InetAddress.getLocalHost().getHostAddress();
+    }
+    public static void set_current_username(String username) throws Uninitialized {
+        if(User.current_id == -1){
+            throw new Uninitialized("CurrentUser not initialized");
+        }
+        User.current_username = username;
+    }
+    public static  String get_current_username() throws Uninitialized {
+        if(User.current_id == -1){
+            throw new Uninitialized("CurrentUser not initialized");
+        }
+        return User.current_username;
+    }
+    public static int get_current_id () throws Uninitialized {
+        if(User.current_id == -1){
+            throw new Uninitialized("CurrentUser not initialized");
+        }
+        return User.current_id;
+    }
+    public static String get_current_ipAddress() throws Uninitialized {
+        if(User.current_id == -1){
+            throw new Uninitialized("CurrentUser not initialized");
+        }
+        return User.current_ipAddress;
+    }
+    /***********************Other users methods***************************/
+    public User(String username,int id,String ipAddress){
+        this.username = username;
+        this.id = id;
+        this.ipAddress = ipAddress;
+        this.lastSeen = new Timestamp(System.currentTimeMillis());
+    }
+    public String get_username() {
+		return this.username;
+	}
+    public int get_id() {
+		return this.id;
+	}
+    public String get_ipAddress() {
+		return this.ipAddress;
+	}
+    public Timestamp get_lastSeen() {
+		return this.lastSeen;
+	}
 }
