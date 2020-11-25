3724918d33fb83827d3fc0ab2ddbaabb9d210d48
nsmaniotto <smaniotto.nathan@gmail.com>
2020-11-25 13:04:23 +0100

    removed reference to ConversationModel which was supposed to be unilateral

--- IDChatSystem/src/main/java/project/insa/idchatsystem/Message.java
+++ IDChatSystem/src/main/java/project/insa/idchatsystem/Message.java
@@ -9,7 +9,6 @@ class Message extends Data {
     private String text;
     private JList<File> files;
     private Timestamp timestamp;
-    private ConversationModel conversationModel;
 
     public Timestamp generateTimeStamp() {
         return null;
