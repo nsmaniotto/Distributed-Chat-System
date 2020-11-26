108da7bc12f363ead56a0a823dc4622fa219e762
nsmaniotto <smaniotto.nathan@gmail.com>
2020-11-26 16:15:21 +0100

    implemented toStream method, allowing a message to be translated to an understandable format

--- IDChatSystem/src/main/java/project/insa/idchatsystem/Message.java
+++ IDChatSystem/src/main/java/project/insa/idchatsystem/Message.java
@@ -2,15 +2,26 @@
 package project.insa.idchatsystem;
 
 import java.io.File;
-import javax.swing.JList;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 
 class Message extends Data {
     private String text;
-    private JList<File> files;
+    private ArrayList<File> files;
     private Timestamp timestamp;
 
     public Timestamp generateTimeStamp() {
         return null;
+    }
+    
+    /**
+     * toStream allows a message to be translated to a certain format
+     * 
+     * @return String - timestamp;message
+     */
+    public String toStream() {
+        String stream = this.timestamp.toString() + ";" + this.text; // For now, files are not considered
+                
+        return stream;
     }
 }
