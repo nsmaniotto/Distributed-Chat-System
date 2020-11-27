0103e9b66eeed3b88c9d62e29f163acc1c9b00b3
nsmaniotto <smaniotto.nathan@gmail.com>
2020-11-27 15:53:34 +0100

    implemented message constructor from network raw input

--- IDChatSystem/src/main/java/project/insa/idchatsystem/Message.java
+++ IDChatSystem/src/main/java/project/insa/idchatsystem/Message.java
@@ -4,11 +4,32 @@ package project.insa.idchatsystem;
 import java.io.File;
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class Message extends Data {
     private String text;
     private ArrayList<File> files;
     private Timestamp timestamp;
+    
+    /**
+     * Constructor called when translating network data to a message
+     * 
+     * @param stream : String - raw input retrieved on the network
+     */
+    public Message(String stream) {
+        // Apply pattern matching to extract timstamp and text message
+        Pattern regex = Pattern.compile("([\\d]+)[;](.+)");
+        Matcher matcher = regex.matcher(stream);
+        
+        if(matcher.find()) {
+            this.timestamp = new Timestamp(Long.parseLong(matcher.group(1)));
+            this.text = matcher.group(2);
+        } else {
+            this.timestamp = new Timestamp(0);
+            this.text = "ERROR: MESSAGE COULD NOT BE DETERMINED";
+        }
+    }
 
     public Timestamp generateTimeStamp() {
         return null;
