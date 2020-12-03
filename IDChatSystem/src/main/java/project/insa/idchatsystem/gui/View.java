0d993f39e55713d5f1c5a44c58f7076b233e466a
nsmaniotto <smaniotto.nathan@gmail.com>
2020-12-03 09:14:03 +0100

    implemented display notification method, relaying the treatment to the chat window

--- IDChatSystem/src/main/java/project/insa/idchatsystem/gui/View.java
+++ IDChatSystem/src/main/java/project/insa/idchatsystem/gui/View.java
@@ -33,4 +33,15 @@ public class View implements Runnable {
         
         this.chat_window.displayMessage(message);
     }
+    
+    /**
+     * Relay the notification treatment and display to the chat window
+     * 
+     * @param message : Message - message of which the current user must be notified
+     */
+    public void displayNotification(Message message) {
+        assert this.chat_window != null : "You are not logged in";
+        
+        this.chat_window.displayNotification(message);
+    }
 }
