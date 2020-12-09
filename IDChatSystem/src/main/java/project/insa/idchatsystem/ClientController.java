a595e85187d2da76ebc1ee6b821d421c485cdc4b
nsmaniotto <smaniotto.nathan@gmail.com>
2020-12-09 11:48:51 +0100

    initialized both view and conversationHandler

--- IDChatSystem/src/main/java/project/insa/idchatsystem/ClientController.java
+++ IDChatSystem/src/main/java/project/insa/idchatsystem/ClientController.java
@@ -1,23 +1,32 @@
 
 package project.insa.idchatsystem;
 
+import project.insa.idchatsystem.Conversation.ConversationHandler;
 import project.insa.idchatsystem.Observers.ConversationHandlerObserver;
 import project.insa.idchatsystem.Observers.UsersStatusObserver;
 import project.insa.idchatsystem.User.distanciel.User;
+import project.insa.idchatsystem.gui.View;
 
 public class ClientController implements ConversationHandlerObserver, UsersStatusObserver {
-    /*private final ConversationHandler conversationHandler;
-    private LocalUserModel localUserModel;
-    private View view;*/
-    //private DistantUserModel centralizedUserModel;
+    private View view;
+    private final ConversationHandler conversationHandler;
+    /*private LocalUserModel localUserModel;
+    private DistantUserModel centralizedUserModel;*/
     
     public ClientController(int id) {
-        /*this.localUserModel = new LocalUserModel();
+        // View init
         this.view = new View();
-        this.conversationHandler = new ConversationHandler();
-        this.conversationHandler.addObserver(this);*/
+        new Thread(view).start();
+        
+        // Conversation handler init
+        this.conversationHandler = ConversationHandler.getInstance();
+        this.conversationHandler.addObserver(this);
+        //new Thread(conversationHandler).start();
+        //this.localUserModel = new LocalUserModel();
     }
 
+    /* USERS STATUS OBSERVER METHODS */
+    
     @Override
     public void offlineUser(User user) {
 
@@ -27,6 +36,8 @@ public class ClientController implements ConversationHandlerObserver, UsersStatu
     public void onlineUser(User user) {
 
     }
+    
+    /* CONVERSATION HANDLER OBSERVER METHODS */
 
     @Override
     public void newMessageReceived(Message receivedMessage) {
