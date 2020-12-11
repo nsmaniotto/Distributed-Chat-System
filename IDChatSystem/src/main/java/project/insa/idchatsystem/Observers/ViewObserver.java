dc67d905c9d293f098660052217ce5531bfd6ea9
nsmaniotto <smaniotto.nathan@gmail.com>
2020-12-11 12:59:35 +0100

    added message sending method

--- IDChatSystem/src/main/java/project/insa/idchatsystem/Observers/ViewObserver.java
+++ IDChatSystem/src/main/java/project/insa/idchatsystem/Observers/ViewObserver.java
@@ -1,5 +1,9 @@
 package project.insa.idchatsystem.Observers;
 
+import project.insa.idchatsystem.Message;
+
 public interface ViewObserver {
     abstract boolean newLogin(String login);
+    
+    abstract void newMessageSending(Message sendingMessage);
 }
