667d828e2cead4cb62225d3c82ab1d239f6858fe
nsmaniotto <smaniotto.nathan@gmail.com>
2020-12-11 13:00:27 +0100

    renamed ChatWindowObservale add/remove observer methods

--- IDChatSystem/src/main/java/project/insa/idchatsystem/Observers/ChatWindowObservable.java
+++ IDChatSystem/src/main/java/project/insa/idchatsystem/Observers/ChatWindowObservable.java
@@ -4,9 +4,9 @@ package project.insa.idchatsystem.Observers;
 import project.insa.idchatsystem.Message;
 
 public interface ChatWindowObservable {
-    public void addViewObserver(ChatWindowObserver observer);
+    public void addChatWindowObserver(ChatWindowObserver observer);
 
-    public void deleteViewObserver(ChatWindowObserver observer);
+    public void deleteChatWindowObserver(ChatWindowObserver observer);
 
     public void notifyObserverSendingMessage(Message sendingMessage);
 }
