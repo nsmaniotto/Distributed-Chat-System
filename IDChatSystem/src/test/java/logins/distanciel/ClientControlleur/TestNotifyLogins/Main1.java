7210259709cf7a69a4bef9e17bca363fd18dd5d4
Rob174 <hn*s['2s%XKmx_8Ndf'M>
2020-12-11 13:06:03 +0100

    Correction oubli de paramètres du constructeur controlleur

--- IDChatSystem/src/test/java/logins/distanciel/ClientControlleur/TestNotifyLogins/Main1.java
+++ IDChatSystem/src/test/java/logins/distanciel/ClientControlleur/TestNotifyLogins/Main1.java
@@ -8,6 +8,6 @@ public class Main1 {
     public static void main (String [] argv) {
         ArrayList<Integer> others1 = new ArrayList<>();
         others1.add(4000);
-        ControllerEncapsulation controller_appli1 = new ControllerEncapsulation(0,2000,2500,others1,2600);
+        ControllerEncapsulation controller_appli1 = new ControllerEncapsulation(0,2000,2500,others1,2600,2601);
     }
 }
