1955719f6841d8e733301b9dd7f8268aa72ab57f
nsmaniotto <smaniotto.nathan@gmail.com>
2020-12-17 11:48:22 +0100

    creating messages table after connecting to previously created database

--- IDChatSystem/src/main/java/project/insa/idchatsystem/database/MessageDatabase.java
+++ IDChatSystem/src/main/java/project/insa/idchatsystem/database/MessageDatabase.java
@@ -20,7 +20,8 @@ public class MessageDatabase {
     private final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";  
     private final int DB_PORT = 3306;
     private final String DB_NAME = "idchatsystem";
-    private final String DB_URL = "jdbc:mysql://localhost:" + DB_PORT;
+    private final String DB_SERVER_URL = "jdbc:mysql://localhost:" + DB_PORT;
+    private final String DB_DATABASE_URL = DB_SERVER_URL + "/" + DB_NAME;
 
     // Database credentials
     private final String USER = "idchatsystem_usr";
@@ -52,22 +53,26 @@ public class MessageDatabase {
             // Register JDBC driver
             Class.forName(this.JDBC_DRIVER);
             
-            // Connect to the database
-            this.connect();
+            // Connect to the local sql server
+            this.connect(DB_SERVER_URL);
         } catch (ClassNotFoundException ex) {
             System.out.println("(MessageDatabase) : EXCEPTION AT REGISTERING JDBC DRIVER : " + ex);
-            Logger.getLogger(MessageDatabase.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
-            System.out.println("(MessageDatabase) : EXCEPTION AT CONNECT : " + ex);
             Logger.getLogger(MessageDatabase.class.getName()).log(Level.SEVERE, null, ex);
         }
         
         this.initDatabase();
     }
     
-    private void connect() throws SQLException {
-        System.out.println("(MessageDatabase) - Connecting to the database...");
-        this.conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+    private void connect(String url) {
+        System.out.println("(MessageDatabase) - Connecting to '" + url + "'");
+        this.conn = null; // Reset the conn each time the method is called 
+        
+        try {
+            this.conn = DriverManager.getConnection(url, USER, PASSWORD);
+        } catch (SQLException ex) {
+            System.out.println("(MessageDatabase) : EXCEPTION AT CONNECT : " + ex);
+            Logger.getLogger(MessageDatabase.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
         if(this.conn != null) {
             System.out.println("(MessageDatabase) - Successfully connected to the database");
@@ -80,11 +85,28 @@ public class MessageDatabase {
         // Create Database if it does not exists
         this.executeUpdate("CREATE DATABASE IF NOT EXISTS " + this.DB_NAME);
         
-        // Create Table
-        //TODO
+        // Connect to the previously created database
+        this.connect(DB_DATABASE_URL);
+        
+        // Create (if not exists) the table which will store the messages
+        this.createMessageTable();
+    }
+    
+    private void createMessageTable() {
+        System.out.println("(MessageDatabase) - Creating the messages table...");
+        
+        this.executeUpdate("CREATE TABLE IF NOT EXISTS messages ("
+                    + "ID INT NOT NULL,"
+                    + "SOURCE_ID INT NOT NULL,"
+                    + "DESTINATION_ID INT NOT NULL,"
+                    + "TEXT TEXT NOT NULL," // TINYTEXT = 255 char, TEXT = 65535 char
+                    + "TIMESTAMP TIMESTAMP NOT NULL,"
+                    + "PRIMARY KEY (ID))");
     }
     
     private void executeUpdate(String update) {
+        System.out.println("(MessageDatabase) - Update : " + update);
+        
         this.createStatement();
         
         if(this.statement != null) {
