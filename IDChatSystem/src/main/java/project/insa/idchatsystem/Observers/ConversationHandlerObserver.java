82ea472a271ce67b77b6902e8614b74d050c60b3
nsmaniotto <smaniotto.nathan@gmail.com>
2021-01-04 16:42:36 +0100

    added notification method to inform retrieved messages

--- IDChatSystem/src/main/java/project/insa/idchatsystem/Observers/ConversationHandlerObserver.java
+++ IDChatSystem/src/main/java/project/insa/idchatsystem/Observers/ConversationHandlerObserver.java
@@ -1,9 +1,12 @@
 
 package project.insa.idchatsystem.Observers;
 
+import java.util.ArrayList;
 import project.insa.idchatsystem.Message;
 
 public interface ConversationHandlerObserver {
     public void newMessageReceived(Message receivedMessage, boolean isCurrentConversation);
     public void newMessageSent(Message sentMessage);
+    
+    public void messagesRetrieved(ArrayList<Message> retrievedMessages);
 }
