93ccc5ef52442f4b331fe65ff6316f6166dce5b1
nsmaniotto <smaniotto.nathan@gmail.com>
2021-02-02 16:32:41 +0100

    modified default message when no correspondent selected

--- IDChatSystem/src/main/java/project/insa/idchatsystem/gui/ChatWindow.java
+++ IDChatSystem/src/main/java/project/insa/idchatsystem/gui/ChatWindow.java
@@ -114,7 +114,7 @@ public class ChatWindow extends Window implements ActionListener, ChatWindowObse
                 );
         this.correspondentPanel.setBackground(Color.white);
 
-        this.correspondentInfoLabel = new JLabel("", JLabel.LEFT);
+        this.correspondentInfoLabel = new JLabel("Select your correspondent!", JLabel.LEFT);
 
         this.chatScrollPane = new JScrollPane(this.chatHistoryPanel);
         this.chatScrollPane.setAutoscrolls(true);
