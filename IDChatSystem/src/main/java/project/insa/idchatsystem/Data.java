1ecafb4255168117b0b20ea9429a689854e6ba89
nsmaniotto <smaniotto.nathan@gmail.com>
2021-02-07 18:22:27 +0100

    added a type factor

--- IDChatSystem/src/main/java/project/insa/idchatsystem/Data.java
+++ IDChatSystem/src/main/java/project/insa/idchatsystem/Data.java
@@ -3,7 +3,7 @@ package project.insa.idchatsystem;
 
 import project.insa.idchatsystem.User.distanciel.User;
 
-public class Data {
+public abstract class Data {
     private User source;
 
     private User destination;
@@ -19,7 +19,18 @@ public class Data {
     public Data(User source, User destination) {
         this.source = source;
         this.destination = destination;
+        this.type = null;
     }
+    
+    public boolean isMessage() {
+        return this.type.equals(Message.TYPE);
+    }
+    
+    public static boolean isInformation(String input) {
+        return input.startsWith(Information.TYPE);
+    }
+    
+    public abstract String toStream();
 
     /* GETTERS/SETTERS */
     public User getSource() {
@@ -36,5 +47,9 @@ public class Data {
     
     public void setDestination(User destination) {
         this.destination = destination;
+    }
+    
+    public void setType(String type) {
+        this.type = type;
     }
 }
